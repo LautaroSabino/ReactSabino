005455d0a60153930a8c830c1114b5e49ff95483
Lautaro Sabino <lautarosabino26@gmail.com>
2022-11-02 15:30:43 -0300

    jugando con los hooks

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,41 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import './ItemListContainer.css'
 
 const ItemListContainer = (props) => {
 
-  const [contador, saveContador] = useState(0)
+  const [count, setCount] = useState(0)
+  const [bool, setBool] = useState(true)
 
   const sumar = () => {
-    saveContador(contador + 1)
+    setCount(count + 1)
   }
 
+  const cambiarEstado = () =>{
+    setBool(!bool)
+  }
+
+  useEffect(()=>{
+    
+    console.log('Se ejecuta siempre por cada render - 1')
+    
+  })
+
+  useEffect(()=>{
+    
+    console.log('Esto es una tarea pesada, llamada a API- 2')
+
+  }, [])
+
+  useEffect(()=>{
+    
+    console.log('Esto es un cambio de estado- 4')
+    console.log(bool)
+
+  }, [bool])
+
+  console.log('Esta es otra tarea - 4')
+  
 
   return (<div>
 
@@ -17,11 +44,15 @@ const ItemListContainer = (props) => {
     </div>
 
     <div>
-      {contador}
+      {count}
     </div>
 
     <div className='btn boton'>
-      <button onClick={sumar}>Sumar</button>
+      <button onClick={sumar}> + </button>
+    </div>
+
+    <div className='btn boton'>
+      <button onClick={cambiarEstado}> Cambiar estado </button>
     </div>
   </div>
   )
