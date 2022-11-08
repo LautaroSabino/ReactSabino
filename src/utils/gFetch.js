3dc567557d8c4260c8660cdab34cb7c43bc0df03
Lautaro Sabino <lautarosabino26@gmail.com>
2022-11-08 13:35:17 -0300

    comenzando con segunda pre entrega

--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -1,26 +1,36 @@
+
+let arrayList = [{
+    ID: '1',
+    nombre: 'Toldo plano',
+    descripción: 'Toldo plano de lona reforzada',
+    stock: 10,
+    foto: 'https://argentinatoldos.com/wp-content/uploads/2021/01/toldo-brazo-invisibles-1024x768.jpg'
+}, {
+    ID: '2',
+    nombre: 'Toldo curvo',
+    descripción: 'Toldo curvo de lona reforzada',
+    stock: 8,
+    foto: 'https://argentinatoldos.com/wp-content/uploads/2021/01/toldo-brazo-invisibles-1024x768.jpg'
+}, {
+    ID: '3',
+    nombre: 'Toldo plano frágil',
+    descripción: 'Toldo plano de lona sencilla',
+    stock: 4,
+    foto: 'https://argentinatoldos.com/wp-content/uploads/2021/01/toldo-brazo-invisibles-1024x768.jpg'
+}, {
+    ID: '4',
+    nombre: 'Toldo roler',
+    descripción: 'Toldo roler de tela blackout',
+    stock: 20,
+    foto: 'https://argentinatoldos.com/wp-content/uploads/2021/01/toldo-brazo-invisibles-1024x768.jpg'
+}]
+
+
 export const gFetch = new Promise((resolve, reject) => {
 
-    let arrayList = [{
-        ID: '1',
-        nombre: 'Toldo plano',
-        descripción: 'Toldo plano de lona reforzada',
-        stock: 10
-    }, {
-        ID: '2',
-        nombre: 'Toldo curvo',
-        descripción: 'Toldo curvo de lona reforzada',
-        stock: 8
-    }, {
-        ID: '3',
-        nombre: 'Toldo plano frágil',
-        descripción: 'Toldo plano de lona sencilla',
-        stock: 4
-    }, {
-        ID: '4',
-        nombre: 'Toldo roler',
-        descripción: 'Toldo roler de tela blackout',
-        stock: 20
-    }]
-    resolve(arrayList)
+    setTimeout(() => {
+        resolve(arrayList)
+        
+    }, 3000);
 
 })
