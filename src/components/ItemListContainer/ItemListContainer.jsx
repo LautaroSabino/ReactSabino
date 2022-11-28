54dca2bb41138ec644ff1e7d732d1aae2a2facbf
Lautaro Sabino <lautarosabino26@gmail.com>
2022-11-28 13:31:38 -0300

    trayendo un objeto de Firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { collection, doc, getDoc, getFirestore } from 'firebase/firestore'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { gFetch } from '../../utils/gFetch'
@@ -7,28 +8,53 @@ import './ItemListContainer.css'
 
 const ItemListContainer = (props) => {
 
- 
+  const [producto, setProducto] = useState({})
   const [products, setProducts] = useState([])
   const {categoriaID} = useParams()
 
+// TRAER UN SOLO OBJETO. 
+// useEffect(()=> {
+//     const dbFirestore = getFirestore()
+//     const queryCollection = doc(dbFirestore, 'Items', '52Ch9ggVEkHdSVNV7Fy2')
+//     getDoc(queryCollection)
+//     .then((doc)=> setProducto({id: doc.id, ... doc.data()}))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log('Siempre'))
+// },[]) 
+
+// TRAER VARIOS OBJETOS
+
+useEffect(()=> {
+  const dbFirestore = getFirestore()
+  const queryCollection = doc(dbFirestore, 'Items', '52Ch9ggVEkHdSVNV7Fy2')
+  getDoc(queryCollection)
+  .then((doc)=> setProducto({id: doc.id, ... doc.data()}))
+  .catch(err => console.log(err))
+  .finally(() => console.log('Siempre'))
+},[]) 
+
+
+
   
-  useEffect(() => {
-    if (categoriaID) {
-      gFetch
-      .then(resp => setProducts(resp.filter(prod => prod.categoria === categoriaID)))
-      .catch(err => console.log(err))
-      .finally(() => console.log('Siempre'))
+  // useEffect(() => {
+  //   if (categoriaID) {
+  //     gFetch
+  //     .then(resp => setProducts(resp.filter(prod => prod.categoria === categoriaID)))
+  //     .catch(err => console.log(err))
+  //     .finally(() => console.log('Siempre'))
       
-    } else (
+  //   } else (
 
-      gFetch
-        .then(resp => setProducts(resp))
-        .catch(err => console.log(err))
-        .finally(() => console.log('Siempre'))
-    )
+  //     gFetch
+  //       .then(resp => setProducts(resp))
+  //       .catch(err => console.log(err))
+  //       .finally(() => console.log('Siempre'))
+  //   )
 
 
-  }, [categoriaID])
+  // }, [categoriaID])
+
+  console.log(producto)
 
   return  (
     <div>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDaZTBbDdZf2CrgGHs9e9dQ7LxGbIxy2b8",
   authDomain: "eccomercesabino.firebaseapp.com",
@@ -16,4 +13,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// export const initFirebase = ()=>
+export const initFirebase = ()=> app
